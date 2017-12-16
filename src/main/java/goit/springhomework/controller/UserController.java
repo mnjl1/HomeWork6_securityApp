697fdafd113b92970f93ff7e9d609dcd4092f5cc
mnjl <d.manzhula@gmail.com>
2017-12-16 23:02:36 +0200

    ver 1.1

--- src/main/java/goit/springhomework/controller/UserController.java
+++ src/main/java/goit/springhomework/controller/UserController.java
@@ -6,6 +6,7 @@ import goit.springhomework.service.UserService;
 import goit.springhomework.validator.UserValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -26,5 +27,34 @@ public class UserController {
         return "registration";
     }
 
-    //public String registration(@ModelAttribute("userForm"), User userForm, );
+    public String registration(@ModelAttribute("userForm") User userForm,
+                               BindingResult bindingResult, Model model){
+
+        userValidator.validate(userForm, bindingResult);
+        if (bindingResult.hasErrors()){
+            return "registration";
+        }
+
+        userService.save(userForm);
+        securityService.autoLogging(userForm.getUserName(), userForm.getPasswordConfirm());
+
+        return "redirect:/welcome";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public  String login (Model model, String error, String logout){
+        if ((error!=null)){
+            model.addAttribute("error", "Invalid password");
+        }
+        if(logout!=null){
+            model.addAttribute("message", "Success logged out.");
+        }
+        return "login";
+    }
+
+    @RequestMapping(value = {"/", "/welcome"}, method = RequestMethod.GET)
+    public String welcome(Model model){
+
+        return "welcome";
+    }
 }
